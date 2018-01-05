498772aecc4ca153baa673d3efb592bfcc32cd5e
jnabywaniec <jacek.nabywaniec@gmail.com>
2018-01-05 21:17:25 +0100

    .

--- Server/src/main/java/agh/to2/dicemaster/game/model/Timer.java
+++ Server/src/main/java/agh/to2/dicemaster/game/model/Timer.java
@@ -1,4 +1,29 @@
 package agh.to2.dicemaster.game.model;
 
+import java.util.concurrent.TimeUnit;
+
 public class Timer {
+
+    private int id;
+
+    public Timer(int id){
+        this.id = id;
+    }
+
+    public int countTime(long seconds){
+
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        }catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getId(){
+        return  id;
+    }
+
+
+
 }
