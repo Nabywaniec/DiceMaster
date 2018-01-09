cb31846864887aaf6ed6f32c062a2ec3ae326e69
jnabywaniec <jacek.nabywaniec@gmail.com>
2018-01-09 13:49:19 +0100

    impl Fabryki

--- Server/src/main/java/agh/to2/dicemaster/game/factory/GameFactory.java
+++ Server/src/main/java/agh/to2/dicemaster/game/factory/GameFactory.java
@@ -1,11 +1,17 @@
 package agh.to2.dicemaster.game.factory;
 
 import agh.to2.dicemaster.common.api.GameConfigDTO;
+import agh.to2.dicemaster.common.api.GameType;
+import agh.to2.dicemaster.game.poker.PokerGame;
 import agh.to2.dicemaster.server.api.Game;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class GameFactory {
     public Game createGame(GameConfigDTO gameConfigDTO) {
+        if(gameConfigDTO.getGameType().equals(GameType.POKER)){
+            return new PokerGame();
+        }
+
         throw new NotImplementedException();
     }
 }
