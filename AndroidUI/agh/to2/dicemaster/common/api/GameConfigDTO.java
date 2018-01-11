3b3d76e9d273d379f244e560359840c60c3049dc
Qube <artur_kubek@outlook.com>
2018-01-11 12:49:00 +0100

    changes

--- AndroidUI/agh/to2/dicemaster/common/api/GameConfigDTO.java
+++ AndroidUI/agh/to2/dicemaster/common/api/GameConfigDTO.java
@@ -1,5 +1,22 @@
 package agh.to2.dicemaster.common.api;
 
 public abstract class GameConfigDTO {
+    public final String tableName;
+    public final int maxPlayers;
+    public final int roundsToWin;
+    public final GameType gameType;
+    public final int hardBotsCount;
+    public final int easyBotsCount;
 
+    public GameConfigDTO(int maxPlayers, int roundsToWin, GameType gameType, int hardBotsCount, int easyBotsCount,String tableName ){
+        this.maxPlayers = maxPlayers;
+        this.roundsToWin = roundsToWin;
+        this.gameType = gameType;
+        this.hardBotsCount = hardBotsCount;
+        this.easyBotsCount = easyBotsCount;
+        this.tableName = tableName;
+    }
+
+    public abstract void fromJSON(String JSON);
+    public abstract void toJSON(String JSON);
 }
