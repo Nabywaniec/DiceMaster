2493f912fc9bc2feac352fc584b66d6a8bed0115
jnabywaniec <jacek.nabywaniec@gmail.com>
2018-01-17 16:49:08 +0100

    add tests

---
+++ Server/src/main/java/agh/to2/dicemaster/game/Test/PokerTests.java
@@ -0,0 +1,13 @@
+package agh.to2.dicemaster.game.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PokerTests {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+      assertEquals(1,1);
+    }
+}
