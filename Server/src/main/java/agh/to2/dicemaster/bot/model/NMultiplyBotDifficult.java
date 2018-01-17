8bc9a058b7310fdf0a1f6741bf41b27401895376
Kamil Kaleta <kamkalet@gmail.com>
2018-01-17 21:25:12 +0100

    name

--- Server/src/main/java/agh/to2/dicemaster/bot/model/NMultiplyBotDifficult.java
+++ Server/src/main/java/agh/to2/dicemaster/bot/model/NMultiplyBotDifficult.java
@@ -4,20 +4,32 @@ package agh.to2.dicemaster.bot.model;
 import agh.to2.dicemaster.bot.DiceInputDTO;
 import agh.to2.dicemaster.bot.DiceOutputDTO;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NMultiplyBotDifficult extends Bot{
 
     @Override
     DiceOutputDTO getDicesToThrow(DiceInputDTO input){
 
+        DiceOutputDTO result = new DiceOutputDTO();
+        result.setNumToFinishGame(input.getNumToFinishGame());
 
-        //FIXME mocked result prepared
-        List<Integer> tmp = input.getMyInput();
-        result.setDicesToThrow(tmp);
+        int dicesAvailabletoThrow = input.getMyInput().size();
+
+        Random r = new Random();
+        int dicesToPutAside = r.nextInt(dicesAvailabletoThrow);
+
+        List<Integer> dicesToThrow = new ArrayList<>(input.getMyInput());
+
+
+
+        result.setDicesToThrow(dicesToThrow);
 
 
         return result;
+
     }
 
 }
