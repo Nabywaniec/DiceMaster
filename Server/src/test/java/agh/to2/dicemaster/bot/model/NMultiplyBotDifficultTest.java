eafd4d761bea163f3cf41a89d79227a7ea884f4e
tomblon <you@example.com>
2018-01-25 11:55:04 +0100

    muldiff test added

--- Server/src/test/java/agh/to2/dicemaster/bot/model/NMultiplyBotDifficultTest.java
+++ Server/src/test/java/agh/to2/dicemaster/bot/model/NMultiplyBotDifficultTest.java
@@ -4,43 +4,67 @@ import agh.to2.dicemaster.bot.BotType;
 import agh.to2.dicemaster.bot.DiceInputDTO;
 import agh.to2.dicemaster.bot.DiceOutputDTO;
 import agh.to2.dicemaster.bot.factory.NMultiplyBotFactory;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Created by kamkalet on 18.01.2018.
+ * Created by AD on 25.01.2018.
  */
-
 class NMultiplyBotDifficultTest {
 
-    NMultiplyBotFactory factory = new NMultiplyBotFactory();
-
+    private NMultiplyBotFactory factory = new NMultiplyBotFactory();
     @Test
-    void getDicesToThrowTest() throws Exception {
-
-        NMultiplyBotDifficult bot = (NMultiplyBotDifficult)
-                factory.createBot(BotType.DIFFICULT);
-
+    void getDicesToThrow() {
+        NMultiplyBotDifficult bot=null;
+        try {
+            bot = (NMultiplyBotDifficult)
+                    factory.createBot(BotType.DIFFICULT);
+        } catch (Exception e) {
+            System.exit(1);
+        }
 
         DiceOutputDTO output = new DiceOutputDTO(30);
-        HashMap<Integer, Integer> dicesReRolled = new HashMap<>();
+        bot.setResult(output);
 
-        //second values are not that essential
-        dicesReRolled.put(1,6);
-        output.setMyInput(Arrays.asList(6,4,4,4,4));
-        output.setDicesToThrow(dicesReRolled);
-        output.setScoreLeft(5);
+        HashMap<Integer, Integer> dicesToThrow = new HashMap<>();
+        dicesToThrow.put(4, 1);
+
 
         DiceInputDTO input = new DiceInputDTO(30);
-        input.setMyInput(Arrays.asList(6,5,1,1,1));
-        input.setDicesRerolled(dicesReRolled);
+        input.setMyInput(Arrays.asList(2, 3, 1, 1, 4));
 
+        testOutputs(dicesToThrow, bot.getDicesToThrow(input).getDicesToThrow());
 
+        HashMap<Integer, Integer> dicesToThrow2 = new HashMap<>();
+        dicesToThrow2.put(1, 1);
+        dicesToThrow2.put(4, 1);
+        input.setMyInput(Arrays.asList(1, 1, 4, 6, 1));
+        input.setScoreToWin(360);
+        testOutputs(dicesToThrow2, bot.getDicesToThrow(input).getDicesToThrow());
 
+    }
 
+    private void testOutputs(HashMap<Integer, Integer> expected ,HashMap<Integer, Integer> result){
+
+        Iterator it = result.entrySet().iterator();
+        Iterator it2 = expected.entrySet().iterator();
+
+        assertEquals(result.size(),  expected.size());
+        Map.Entry pair;
+        Map.Entry pair2;
+        for (int j = 0; j < result.size(); j++) {
+            pair = (Map.Entry) it.next();
+            pair2 = (Map.Entry) it2.next();
+            assertEquals((Integer)pair.getKey(), (Integer)pair2.getKey());
+        }
 
 
     }
+
 }
